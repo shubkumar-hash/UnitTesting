302c7173a9fe375ec6edafc775e123b8518c0bb8
shubham <shubkumar@griddynamics.com>
2026-02-26 15:04:48 +0530

    resolve PersonRepoTest

--- src/test/java/com/example/UnitTesting/PersonRepoTest/PersonRepoTest.java
+++ src/test/java/com/example/UnitTesting/PersonRepoTest/PersonRepoTest.java
@@ -2,23 +2,23 @@ package com.example.UnitTesting.PersonRepoTest;
 
 import com.example.UnitTesting.model.Person;
 import com.example.UnitTesting.repo.PersonRepo;
-import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@DataJpaTest
-public class PersonRepoTest {
+@SpringBootTest
+class PersonRepoTest {
 
     @Autowired
     private PersonRepo personRepo;
 
     @Test
-    public void testExistsById(){
+    void testExistsById() {
         Person person = new Person(null, "Aman", "Ranchi");
-        person = personRepo.save(person);
-        boolean exists = personRepo.existsById(person.getId());
+        Person savedPerson = personRepo.save(person);
+        boolean exists = personRepo.existsById(savedPerson.getId());
         assertThat(exists).isTrue();
     }
 }
